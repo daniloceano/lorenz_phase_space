3e1b0a99e0ee1d01bd7757a67f4687e02c4e30b6
daniloceano <danilo.oceano@gmail.com>
2024-01-02 11:10:14 -0300

    displays README.md at Pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+with open('README.md', encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
 VERSION = '0.0.1'
 DESCRIPTION = 'Visualization tool designed to analyze and illustrate the Lorenz Energy Cycle for atmospheric science.'
-LONG_DESCRIPTION = 'This tool offers a unique perspective for studying the intricate processes governing atmospheric energetics and instability mechanisms. It visualizes the transformation and exchange of energy within the atmosphere,specifically focusing on the interactions between kinetic and potential energy forms as conceptualized by Edward Lorenz.'
 
 setup(
     name="lorenz_phase_space",
     version=VERSION,
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
     author="Danilo Couto de Souza",
     author_email="danilo.oceano@gmail.com",
     license='MIT',
