55bd1e8e314da8411350ee4baf4264a8469e672b
daniloceano <danilo.oceano@gmail.com>
2024-02-28 15:49:15 -0300

    fixed import

--- tests/test_lps.py
+++ tests/test_lps.py
@@ -1,7 +1,6 @@
 import unittest
 import numpy as np
-import pandas as pd
-from LPS import LorenzPhaseSpace, get_max_min_values
+from ..LPS import LorenzPhaseSpace
 
 class TestLorenzPhaseSpace(unittest.TestCase):
 
